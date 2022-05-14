099bb763aab0720e4b34020b2fca1970649f05fb
AhmedRafique <70059941+AhmedRafique@users.noreply.github.com>
2022-05-14 21:53:44 +0200

    Add files via upload

--- MCAL/DIO/DIO_program.c
+++ MCAL/DIO/DIO_program.c
@@ -14,60 +14,6 @@
 #include "DIO_private.h"
 #include "DIO_config.h"
 
-u8 DIO_u8SetPinDirection(u8 Copy_u8Port,u8 Copy_u8Pin,u8 Copy_u8Direction)
-{
-	u8 Local_u8ErrorState = 0;
-	if(Copy_u8Pin <= DIO_u8PIN7)
-	{
-		if(Copy_u8Direction == DIO_u8PIN_INPUT)
-		{
-			switch(Copy_u8Port)
-			{
-			case DIO_u8PORTA: CLR_BIT(DDRA,Copy_u8Pin); break;
-			case DIO_u8PORTB: CLR_BIT(DDRB,Copy_u8Pin); break;
-			case DIO_u8PORTC: CLR_BIT(DDRC,Copy_u8Pin); break;
-			case DIO_u8PORTD: CLR_BIT(DDRD,Copy_u8Pin); break;
-			default: Local_u8ErrorState = 1; break;
-			}
-		}
-		else if(Copy_u8Direction == DIO_u8PIN_OUTPUT)
-		{
-			switch(Copy_u8Port)
-			{
-			case DIO_u8PORTA: SET_BIT(DDRA,Copy_u8Pin); break;
-			case DIO_u8PORTB: SET_BIT(DDRB,Copy_u8Pin); break;
-			case DIO_u8PORTC: SET_BIT(DDRC,Copy_u8Pin); break;
-			case DIO_u8PORTD: SET_BIT(DDRD,Copy_u8Pin); break;
-			default: Local_u8ErrorState = 1; break;
-			}
-		}
-		else
-		{
-			Local_u8ErrorState = 1;
-		}
-	}
-	else
-	{
-		Local_u8ErrorState = 1;
-	}
-
-	return Local_u8ErrorState;
-}
-
-u8 DIO_u8SetPortDirection(u8 Copy_u8Port,u8 Copy_u8Direction)
-{
-	u8 Local_u8ErrorState = 0;
-	switch(Copy_u8Port)
-	{
-	case DIO_u8PORTA : DDRA = Copy_u8Direction; break;
-	case DIO_u8PORTB : DDRB = Copy_u8Direction; break;
-	case DIO_u8PORTC : DDRC = Copy_u8Direction; break;
-	case DIO_u8PORTD : DDRD = Copy_u8Direction; break;
-	default: Local_u8ErrorState = 1;
-	}
-	return Local_u8ErrorState;
-}
-
 u8 DIO_u8SetPinValue(u8 Copy_u8Port,u8 Copy_u8Pin,u8 Copy_u8Value)
 {
 	u8 Local_u8ErrorState = 0;
